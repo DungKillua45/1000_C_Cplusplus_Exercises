ac4ad078cc1bee4121df1a7a6ed09ed85f10047f
DungKillua45 <89728121+DungKillua45@users.noreply.github.com>
2021-09-02 10:18:50 +0700

    Add files via upload

---
+++ Chuong 1- Luu do thuat toan/Bai 25.cpp
@@ -0,0 +1,31 @@
+/*
+	Exercise 25:
+	Tinh tong cac uoc so chan cua n
+*/
+#include <iostream>
+#include<math.h>
+#include<conio.h>
+using namespace std;
+int main(){
+	int n, sum=0;
+	comeback: do{
+		cout<<"Nhap n>=1 : ";
+		cin>>n;
+		if(n<1){
+			cout<<"Nhap lai n>=1 !\n";
+		}
+	}while(n<1);
+	for(int i=1; i<=n; i++){
+		if(n%i==0 && i%2==0){
+			sum += i;
+		}
+	}
+	cout<<"Tong tat ca cac uoc so chan cua n la: "<<sum;
+	char tieptuc;
+	cout<<"\nDo you want to continue?\n";
+	tieptuc=getch();
+	if(tieptuc=='y'||tieptuc=='Y'){
+		goto comeback;
+	}
+	return 0;
+}
