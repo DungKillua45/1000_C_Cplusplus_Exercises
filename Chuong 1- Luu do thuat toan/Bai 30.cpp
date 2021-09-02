0d6bdf847baaebd920b28c89841265a99bbc927e
DungKillua45 <89728121+DungKillua45@users.noreply.github.com>
2021-09-02 16:47:02 +0700

    Add files via upload

---
+++ Chuong 1- Luu do thuat toan/Bai 30.cpp
@@ -0,0 +1,29 @@
+/*
+	Exercise 30:
+	Kiem tra n co phai la so hoan thien khong
+*/
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+using namespace std;
+void ketqua(int n){
+	int sum=0;
+	cout<<"Nhap n: ";
+	cin>>n;
+	for(int i=1; i<n; i++){
+		if(n%i==0){
+			sum += i;
+		}
+	}
+	cout<<"Tong tat cac cac uoc khong bao gom n la: "<<sum<<endl;
+	if(sum==n){
+		cout<<"=> n la so hoan thien";
+	}else{
+		cout<<"=> n khong phai la so hoan thien";
+	}
+}
+int main(){
+	int n;
+	ketqua(n);
+	return 0;
+}
